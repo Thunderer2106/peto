ef38bca7a27eb634878cd9f2898560eceefce055
Akash2004 <114282536+Thunderer2106@users.noreply.github.com>
2023-03-16 23:09:08 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      signature: ["Great Vibes"],
+      sign:["Sanchez"],
+      s1:["Cormorant Garamond"],
+      s2:['Gentium Plus'],
+      s3:['Lobster']
+    },
+  },
+  plugins: [],
+};
